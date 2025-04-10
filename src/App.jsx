3bd795b7495fe4730a6feb76349bae9dcb57f4a7
Juanjopalma <juanjose.palma@sdggroup.com>
2025-04-10 11:04:21 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Dashboard React Js</h1>
+    </>
+  )
+}
+
+export default App
